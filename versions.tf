35ebcfae051bcb45eac65c65e723c51503f019bd
lotus.noir <lotus.noir@gmail.com>
2023-10-24 09:42:51 +0200

    change versions

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.3.7"
+  required_version = ">= 1.3"
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = ">= 2.17.0"
+      version = ">= 2.10.0"
     }
   }
 }
